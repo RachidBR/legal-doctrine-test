d5762a5ad1f507248c6797b256292e43775693a0
unknown <rachid.bouadi68@gmail.com>
2022-10-16 22:03:01 +0200

    fixed UI bug concerning product components

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -4,19 +4,17 @@ type Props = {
   title: string;
   price: string;
   description: string;
-  rating: number;
 };
 
 const Product = (props: Props) => {
-  const { imageSrc, title, price, description, rating } = props;
+  const { imageSrc, title, price, description } = props;
   return (
     <article className={styles.product}>
       <section className={styles.product_picture_section}>
 
         <img
           src={imageSrc}
-          height={65}
-          width={65}
+         
           alt="product picture"
           className={styles.product_picture}
           />
@@ -30,14 +28,12 @@ const Product = (props: Props) => {
           </section>
           <section className={styles.product_secondaryInformation}>
             <section className={styles.product_rating}>
-              {[...Array(rating)].map((rating: number, index: number) => (
-                <span>⭐</span>
-              ))}
+                ⭐⭐⭐⭐⭐
             </section>
             <b className={styles.product_price}>£ {price}</b>
           </section>
         </article>
-      <button type="submit">Add to cart</button>
+      <button type="submit">ADD TO CART</button>
       </main>
     </article>
   );
--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -8,24 +8,21 @@ const ProductList = () => {
       <Product
         title="Whole bread"
         description="Made in Algiers and destinated to the whole world"
-        price={"1.00"}
-        rating={5}
+        price="1.00"
         imageSrc="/assets/pictures/bread.jpg"
       />
-      {/* <Product
+      <Product
         title="Fresh Suiss milk"
         description="Semi skimmed milk that comes straight from the alpes farmers"
-        price={1.15}
-        rating={5}
-        imageSrc=""
+        price="1.15"
+        imageSrc="/assets/pictures/milk.jpg"
       />
       <Product
         title="Butter"
         description="Produced by us to insure a high quality butter"
-        price={0.8}
-        rating={5}
-        imageSrc=""
-      /> */}
+        price="0.8"
+        imageSrc="/assets/pictures/butter.jpg"
+      />
     </article>
   );
 };
