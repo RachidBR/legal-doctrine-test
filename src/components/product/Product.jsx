b94e1e269981aeba6d6e67af16d784f42d1531fc
unknown <rachid.bouadi68@gmail.com>
2022-10-16 17:45:25 +0200

    initial commit

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const Product = () => {
+  return (
+    <article>
+
+    <main>
+
+    </main>
+    <footer>
+        
+    </footer>
+    </article>
+  )
+}
+
+export default Product
